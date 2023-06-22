9f2189adf3d31875b0bae4f4c71f684a2a79d520
Mark Finger <markfinger@gmail.com>
2023-06-22 18:23:36 +1000

    Removed legacy TGM content. See: tgm-archive GH org + site

--- recent-work/gmo-food/js/libs/utils.js
+++
@@ -1,5 +0,0 @@
-define({
-    getScrollY: function() {
-      return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    }
-});
